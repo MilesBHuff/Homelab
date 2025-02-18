f3f6b33ad7b4fbcac83fe66de48108577620e833
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2025-02-17 21:26:00 -0500

    Added a script to configure TrueNAS's boot pool.

---
+++ settings/configure-truenas-pool.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+## Configure TrueNAS's default pool's settings
+
+## Make sure we're root
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: This script must be run as root." >&2
+    exit 1
+fi
+
+## Get environment variables
+ENV_FILE='../env.sh'; if [[ -f "$ENV_FILE" ]]; then source ../env.sh; else echo "ERROR: Missing '$ENV_FILE'."; exit -1; fi
+if [[ \
+    -z "$ENV_ACCEPTABLE_DATA_LOSS_SECONDS" ||\
+    -z "$ENV_SLOWEST_HDD_MAX_SPEED_MBPS" ||\
+    -z "$ENV_SMALL_FILE_THRESHOLD"
+]]; then
+    echo "ERROR: Missing variables in '$ENV_FILE'!" >&2
+    exit 3
+fi
+
+## Configure TrueNAS's boot pool
+POOL_NAME='boot-pool'
+zfs set atime=off "$POOL_NAME"
+zfs set compression=lz4 "$POOL_NAME"
+zfs set logbias=throughput "$POOL_NAME"
+zfs set recordsize="$ENV_SMALL_FILE_THRESHOLD" "$POOL_NAME"
+exit 0
