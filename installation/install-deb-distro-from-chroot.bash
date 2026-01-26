008140c5815283081605c2952930a0a1ee43671a
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2026-01-25 21:07:24 -0500

    Added a ZFS trim timer

--- installation/install-deb-distro-from-chroot.bash
+++ installation/install-deb-distro-from-chroot.bash
@@ -288,7 +288,27 @@ echo 'Make sure to import your pools with `import -d /dev/disk/by-id`! Else, you
 ## Configure trim/discard
 echo ':: Scheduling trim...'
 systemctl enable fstrim.timer ## Auto-trims everything in /etc/fstab
-#TODO: Auto-trim zpools, too.
+cat > /etc/systemd/system/zfstrim.service <<EOF
+[Unit]
+Description=Trim ZFS pools
+After=zfs.target
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/zpool trim -a
+IOSchedulingClass=idle
+EOF
+cat > /etc/systemd/system/zfstrim.timer <<EOF
+[Unit]
+Description=Periodic ZFS trim
+[Timer]
+OnCalendar=*-*-* 03:00
+Persistent=true
+AccuracySec=1min
+[Install]
+WantedBy=timers.target
+EOF
+systemctl enable zfstrim.timer
+
 
 ################################################################################
 ## MOUNT OPTIONS
